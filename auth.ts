646770616796c2e0b099f5cbf64ac25a4126e9b5
Oliver Götz <goetzoliver89@gmail.com>
2025-01-17 17:36:05 +0100

    moved to drizzle

--- auth.ts
+++ auth.ts
@@ -1,11 +1,23 @@
 import NextAuth from "next-auth";
-import { PrismaAdapter } from "@auth/prisma-adapter";
-import { prisma } from "./lib/prisma";
 import Google from "next-auth/providers/google";
 import Facebook from "next-auth/providers/facebook";
 import Resend from "next-auth/providers/resend";
+import { DrizzleAdapter } from "@auth/drizzle-adapter";
+import {
+  accounts,
+  sessions,
+  users,
+  verificationTokens,
+} from "./drizzle/schema";
+import { db } from "./drizzle/drizzle";
+
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  adapter: PrismaAdapter(prisma),
+  adapter: DrizzleAdapter(db, {
+    usersTable: users,
+    accountsTable: accounts,
+    sessionsTable: sessions,
+    verificationTokensTable: verificationTokens,
+  }),
   providers: [
     Google,
     Facebook,
@@ -14,4 +26,9 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     }),
   ],
   //secret: config.env.auth.secret,
+  pages: {
+    signIn: "/auth/signin",
+    signOut: "/auth/signout",
+    error: "/auth/error",
+  },
 });
