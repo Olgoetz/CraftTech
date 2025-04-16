bc012fc483bbe06da0af03d133ab2a8218cee2d6
Oliver Götz <goetzoliver89@gmail.com>
2025-04-16 08:02:05 +0300

    upload functionlity fixed

--- lib/actions/upload.actions.ts
+++ lib/actions/upload.actions.ts
@@ -93,7 +93,7 @@ export const uploadFile = async (file: File, fileType: string) => {
   logger("info", "File upload finished", preSignedURL.split("?")[0]);
 
   if (!result.ok) {
-    logger("error", "Failed to upload file", result);
+    logger("error", "Failed to upload file", await result.json());
     throw new Error("Failed to upload file");
   }
   await upsertFile({ fileName: file.name, fileType, fileUrl: preSignedURL });
